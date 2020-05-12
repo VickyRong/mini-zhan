1c6b19efdc84c4225231386343255515b6f896dd
VickRong <515020842@qq.com>
2020-05-12 14:07:49 +0800

    显示属性

--- src/pages/Workspace/workspaceRight/index.jsx
+++ src/pages/Workspace/workspaceRight/index.jsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import styles from './index.less'
+import { connect } from 'dva';
+
+@connect(({workspace }) => ({
+    workspace:workspace,
+    currentComp:workspace.currentComp
+}))
 
 class WorkspaceRight extends React.Component {
+
     render(){
+         let { currentComp } = this.props
         return(
             <div className = { styles.right_wrap } >
                 属性面板
+                <p>组件名： {currentComp.name} </p>
             </div>
         )
     }
